67efa4ea78ea7297f133b17817c3e881c95f328f
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-07-16 13:41:51 -0400

    created the generate logo function and did some debugging

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path');
-const { Triangle, Square, Circle } = require('./lib/shapes');
+//const path = require('path');
+const { Triangle, Circle, Square } = require('./lib/shapes');
 
 
 const questions = [
     {
         type: 'input',
-        name: 'title',
+        name: 'text',
         message: 'Please enter up to 3 characters'
     },
     {
         type: 'input',
-        name: 'title',
+        name: 'textcolor',
         message: 'Please enter a color or hexadecimal number'
     },
     {
         type: 'input',
-        name: 'title',
+        name: 'shape',
         message: 'please choose a shape',
         choices: [
             'circle',
@@ -27,31 +27,61 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'title',
+        name: 'shapecolor',
         message: 'Enter a shape color or a hexadecimal number'
     },
-   
-];
 
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
+];
 
 
 function init() {
     inquirer.prompt(questions)
-    .then((responses) => {
-        const text = res.text;
-        const textColor = res.textcolor;
-        const shape = res.shape;
-        const shapeColor= res.shapecolor;
+        .then((res) => {
+            const text = res.text;
+            const textColor = res.textcolor;
+            const shape = res.shape;
+            const shapeColor = res.shapecolor;
 
-        generateSVG(text, textColor, shape, shapeColor);
-        console.log('Create your logo!');
-        writeToFile('new.svg', ({ ...responses}));
-    }).catch((err) => console.info(err));
+            generateSVG(text, textColor, shape, shapeColor);
+            console.log('Create your logo!');
+        }).catch((err) => console.info(err));
+}
+
+function generateSVG(text, textColor, shape, shapeColor) {
+    if (shape === 'Triangle') {
+        const triangle = new Triangle(text, textColor, shapeColor);
+        return fs.writeFile('logo.svg', triangle.render(), err => {
+           if (err){
+            console.log(err);
+           } else {
+            console.log('You have generated a logo');
+           }
+        });
+        } else {
+
+if (shape === 'Square') {
+    const square = new Square(text, textColor, shapeColor);
+    return fs.writeFile('logo.svg', square.render(), err => {
+        if (err){
+            console.log(err);
+           } else {
+            console.log('You have generated a logo');
+           }
+        });
+        }
     }
 
-    
+if (shape === 'Circle') {
+    const circle = new Circle(text, textColor, shapeColor);
+    return fs.writeFile('logo.svg', circle.render(), err => {
+        if (err){
+            console.log(err);
+           } else {
+            console.log('You have generated a logo');
+           }
+        });
+        }
 
-    init();
+    }
+
+init();
